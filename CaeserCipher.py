475046271861908a2a08c703dc761ece805f6fea
Ikirustar <ikirustar@gmail.com>
2025-02-15 03:31:49 -0500

    refactured code - Added main.py - Rewrote CeaserCipher.py - Rewrote TaskCreator.py

--- CaeserCipher.py
+++ CaeserCipher.py
@@ -1,14 +1,19 @@
-# functions
+# Constants
+
+ALPHABET = [chr(i) for i in range(97, 123)]
+# Functions
+
+
 def encrypt(ms, s):
     psList = list(ms)
     encryptedMessage = ""
     for i in range(len(psList)):
-        if psList[i] in alphabet:
+        if psList[i] in ALPHABET:
             if psList[i].islower():
-                encryptedMessage += alphabet[(
-                    alphabet.index(psList[i]) + int(s)) % 26]
+                encryptedMessage += ALPHABET[(
+                    ALPHABET.index(psList[i]) + int(s)) % 26]
             else:
-                encryptedMessage += alphabet[(alphabet.index(
+                encryptedMessage += ALPHABET[(ALPHABET.index(
                     psList[i].lower()) + int(s)) % 26].upper()
         else:
             encryptedMessage += psList[i]
@@ -20,12 +25,12 @@ def decrypt(ms, k):
     msList = list(ms)
     decryptedMessage = ""
     for i in range(len(msList)):
-        if msList[i] in alphabet:
+        if msList[i] in ALPHABET:
             if msList[i].islower():
-                decryptedMessage += alphabet[(
-                    alphabet.index(msList[i]) - int(k)) % 26]
+                decryptedMessage += ALPHABET[(
+                    ALPHABET.index(msList[i]) - int(k)) % 26]
             else:
-                decryptedMessage += alphabet[(alphabet.index(
+                decryptedMessage += ALPHABET[(ALPHABET.index(
                     msList[i].lower()) - int(k)) % 26].upper()
         else:
             decryptedMessage += msList[i]
@@ -33,27 +38,38 @@ def decrypt(ms, k):
     return decryptedMessage
 
 
-# variables
-alphabet = [chr(i) for i in range(97, 123)]
-message = []
-
-# menu selection
-programRun = True
-while programRun:
-
+def display_menu():
     print("------------------------ CipherTask ------------------------")
     print("- 1. Encrypt                                               -")
     print("- 2. Decrypt                                               -")
     print("- 3. Quit                                                  -")
     print("------------------------------------------------------------\n")
 
-    choice = input("Pick an option: ")
-    if choice == "1":
-        message = input("Enter message: ")
-        shift = input("Enter number of letters to shift: ")
-        print("\nEncrypted message: ", encrypt(message, shift))
-        programRun = False
-    elif choice == "2":
-        message = input("Enter message: ")
-        key = input("Enter key: ")
-        print("\nDecrypted message: ", decrypt(message, key))
+
+def main():
+    programRun = True
+    while programRun:
+        display_menu()
+        choice = input("Pick an option: ")
+        if choice == "1":
+            message = input("Enter message: ")
+            shift = input("Enter number of letters to shift: ")
+            if shift.isdigit():
+                print("\nEncrypted message: ", encrypt(message, shift))
+            else:
+                print("Invalid input. Please enter a valid number for the shift.")
+        elif choice == "2":
+            message = input("Enter message: ")
+            key = input("Enter key: ")
+            if key.isdigit():
+                print("\nDecrypted message: ", decrypt(message, key))
+            else:
+                print("Invalid input. Please enter a valid number for the key.")
+        elif choice == "3":
+            programRun = False
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+
+if __name__ == "__main__":
+    main()
--- TaskCreator.py
+++ TaskCreator.py
@@ -5,59 +5,73 @@ def addTask(taskList):
 
 
 def removeTask(taskList):
-    taskList.pop(int(input("Enter task number to remove: "))-1)
+    try:
+        taskList.pop(int(input("Enter task number to remove: "))-1)
+    except (ValueError, IndexError):
+        print("Invalid task number.")
     return taskList
 
 
 def markComplete(taskList, tasksCompleted):
-    completedTask = int(input("Enter task number to mark complete: "))-1
-    wordCrossed = ''
-    for c in taskList[completedTask]:
-        wordCrossed = wordCrossed + c + '\u0336'
-
-    tasksCompleted.append(wordCrossed)
-    taskList.pop(completedTask)
-
+    try:
+        completedTask = int(input("Enter task number to mark complete: "))-1
+        wordCrossed = ''.join([c + '\u0336' for c in taskList[completedTask]])
+        tasksCompleted.append(wordCrossed)
+        taskList.pop(completedTask)
+    except (ValueError, IndexError):
+        print("Invalid task number.")
     return taskList, tasksCompleted
 
 
-# variables
-tasks = []
-tasksCompleted = []
-title = "------------------------ CipherTask ------------------------"
-titleCharCnt = len(title)
+def display_tasks(tasks, tasksCompleted, titleCharCnt):
+    print("------------------------ Task List -------------------------")
+    if not tasks:
+        print("-                                                          -")
+    for i, task in enumerate(tasks):
+        print(f"- {i+1}. {task}{' ' * (titleCharCnt-6-len(task))}-")
+    print("------------------------------------------------------------\n")
+    print("------------------------ Completed -------------------------")
+    if not tasksCompleted:
+        print("-                                                          -")
+    for task in tasksCompleted:
+        task_length = len(task) // 2  # Each character is followed by a strikethrough character
+        print(f"- {task}{' ' * (titleCharCnt-3-task_length)}-")
+    print("------------------------------------------------------------\n")
 
-# menu selection
-programRun = True
-while programRun:
+
+def display_menu():
     print("------------------------ CipherTask ------------------------")
     print("- 1. Add                                                   -")
     print("- 2. Remove                                                -")
     print("- 3. Mark Complete                                         -")
     print("- 4. Quit                                                  -")
     print("------------------------------------------------------------\n")
-    print("------------------------ Task List -------------------------")
-    if tasks == []:
-        print("-                                                          -")
-    for i in range(len(tasks)):
-        print("- "+str(i+1) + ". " + tasks[i], end="")
-        print(" "*(titleCharCnt-4-len(tasks[i])) + "-")
-    print("------------------------------------------------------------\n")
-    print("------------------------ Completed -------------------------")
-    if tasksCompleted == []:
-        print("-                                                          -")
-    for i in range(len(tasksCompleted)):
-        print("- "+tasksCompleted[i], end="")
-        print(" "*(titleCharCnt+1-len(tasksCompleted[i])) + "-")
-    print("------------------------------------------------------------\n")
 
-    choice = input("Pick an option: ")
-    if choice == "1":
-        tasks = addTask(tasks)
-    elif choice == "2":
-        tasks = removeTask(tasks)
-    elif choice == "3":
-        tasks, tasksCompleted = markComplete(tasks, tasksCompleted)
-    elif choice == "4":
-        programRun = False
-    print("\n")
+
+def main():
+    tasks = []
+    tasksCompleted = []
+    title = "------------------------ CipherTask ------------------------"
+    titleCharCnt = len(title)
+
+    programRun = True
+    while programRun:
+        display_menu()
+        display_tasks(tasks, tasksCompleted, titleCharCnt)
+
+        choice = input("Pick an option: ")
+        if choice == "1":
+            tasks = addTask(tasks)
+        elif choice == "2":
+            tasks = removeTask(tasks)
+        elif choice == "3":
+            tasks, tasksCompleted = markComplete(tasks, tasksCompleted)
+        elif choice == "4":
+            programRun = False
+        else:
+            print("Invalid choice. Please select a valid option.")
+        print("\n")
+
+
+if __name__ == "__main__":
+    main()
