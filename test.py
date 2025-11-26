5359a43449043053509612a646550e170406e889
ikirustar 74 <Ikirustar>
2025-11-26 04:29:42 -0500

    cleaned up repo to publish

--- test.py
+++
@@ -1,9 +0,0 @@
-import keyring
-
-# Set password
-keyring.set_password("chris", "pass", "12345")
-
-# Retrieve it later
-password = keyring.get_password("chris", "pass")
-
-print(password)  # securely retrieved from system store
